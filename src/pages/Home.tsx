58c6f5c34d69fa613e244bbd3f7229d134072f05
Devin Hua <devinhua004@gmail.com>
2025-12-03 03:48:39 -0800

    (feat): cleaned up tags, mapped each tag to unique color, added hover effects

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,7 +6,7 @@ export const Home = () => {
       <div className="absolute inset-0 bg-black/30 backdrop-blur-sm">
         <div className="flex flex-col justify-center min-h-screen items-center">
           <h1 className="text-9xl font-bold text-white text-shadow-lg font-serif">CHOODIE</h1>
-          <p className="text-6xl text-white font-serif text-shadow-lg mt-4">Find the best resturants near you</p>
+          <p className="text-6xl text-white font-serif text-shadow-lg mt-4">Find the best restaurants near you</p>
           <Link to="/LastBiteStanding">
             <button className="w-32 h-24 bg-green-500 border border-black/30 text-white font-semibold rounded-xl text-5xl font-serif mt-8 hover:bg-green-400 active:scale-105">
               GO
--- src/pages/LastBiteStanding.tsx
+++ src/pages/LastBiteStanding.tsx
@@ -20,6 +20,73 @@ export const LastBiteStanding = () => {
   const [tournament, setTournament] = useState<Restaurant[]>([]);
   const [winner, setWinner] = useState(false);
   const hasFetched = useRef(false);
+  const [tagColorMap, setTagColorMap] = useState<Record<string, string>>({});
+
+  const colors = [
+    "bg-red-300",
+    "bg-red-400",
+    "bg-red-500",
+    "bg-green-300",
+    "bg-green-400",
+    "bg-green-500",
+    "bg-blue-300",
+    "bg-blue-400",
+    "bg-blue-500",
+    "bg-yellow-300",
+    "bg-yellow-400",
+    "bg-yellow-500",
+    "bg-purple-300",
+    "bg-purple-400",
+    "bg-purple-500",
+    "bg-pink-300",
+    "bg-pink-400",
+    "bg-pink-500",
+    "bg-orange-300",
+    "bg-orange-400",
+    "bg-orange-500",
+    "bg-indigo-300",
+    "bg-indigo-400",
+    "bg-indigo-500",
+    "bg-teal-300",
+    "bg-teal-400",
+    "bg-teal-500",
+    "bg-cyan-300",
+    "bg-cyan-400",
+    "bg-cyan-500",
+    "bg-lime-300",
+    "bg-lime-400",
+    "bg-lime-500",
+    "bg-rose-300",
+    "bg-rose-400",
+    "bg-rose-500",
+    "bg-fuchsia-300",
+    "bg-fuchsia-400",
+    "bg-fuchsia-500",
+    "bg-violet-300",
+    "bg-violet-400",
+    "bg-violet-500",
+    "bg-amber-300",
+    "bg-amber-400",
+    "bg-amber-500",
+    "bg-sky-300",
+    "bg-sky-400",
+    "bg-sky-500",
+    "bg-emerald-300",
+    "bg-emerald-400",
+    "bg-emerald-500",
+    "bg-gray-300",
+    "bg-gray-400",
+    "bg-gray-500",
+    "bg-stone-300",
+    "bg-stone-400",
+    "bg-stone-500",
+    "bg-zinc-300",
+    "bg-zinc-400",
+    "bg-zinc-500",
+    "bg-neutral-300",
+    "bg-neutral-400",
+    "bg-neutral-500",
+  ];
 
   useEffect(() => {
     if (TEST_MODE) {
@@ -27,23 +94,39 @@ export const LastBiteStanding = () => {
       function restConverter(places: any[]): Restaurant[] {
         return places.map((p) => ({
           title: p.displayName.text,
-          image: "choodie.png",
+          image: TEST_MODE ? "choodie.png" : p.image,
           rating: "⭐".repeat(Math.round(p.rating || 0)),
-          tags: p.types?.slice(0, 2).join(", ") || "Restaurant",
+          tags: p.types
+            ?.slice(0, 2)
+            .filter((item: string) => item !== "restaurant")
+            .join(", ")
+            .replaceAll("_", " ")
+            .replaceAll(" restaurant", ""),
           address: p.formattedAddress,
           priceLevel: p.priceLevel,
         }));
       }
-
       const converted = restConverter(testData.places);
       setRestaurants(converted);
       setTournament(converted);
       setLoading(false);
     } else if (location && !hasFetched.current) {
+      // converts Restaurant[] tags to be usable
       fetchNearbyRestaurants(location.lat, location.lon)
         .then((data) => {
-          setRestaurants(data);
-          setTournament(data);
+          const cleanedData = data.map((restaurant) => {
+            const cleanedTags = restaurant.tags
+              .split(", ")
+              .filter((item: string) => item !== "restaurant")
+              .map((tag) =>
+                tag.replaceAll("_", " ").replaceAll(" restaurant", "").trim()
+              )
+              .join(", ");
+
+            return { ...restaurant, tags: cleanedTags };
+          });
+          setRestaurants(cleanedData);
+          setTournament(cleanedData);
           setLoading(false);
           hasFetched.current = true;
         })
@@ -77,6 +160,27 @@ export const LastBiteStanding = () => {
       setLoading(false);
     }
   }, []);
+
+  // Pick Between Card 1 & Card 2
+  const handleSelect = (indexToRemove: number) => {
+    setTournament((prevArray) => {
+      const itemToRemove = prevArray[indexToRemove].title;
+      const updatedArray = prevArray.filter(
+        (element) => element.title !== itemToRemove
+      );
+
+      if (updatedArray.length === 1) {
+        setWinner(true);
+      } else {
+        const newLength = updatedArray.length;
+
+        setFirstIndex((prev) => Math.min(prev, newLength - 1));
+        setSecondIndex((prev) => Math.min(prev, newLength - 1));
+      }
+      // return updatedArray to update the state for tournament
+      return updatedArray;
+    });
+  };
 
   // Confetti effect from canvas-confetti
   useEffect(() => {
@@ -116,26 +220,22 @@ export const LastBiteStanding = () => {
     }
   }, [winner]);
 
-  // Pick Between Card 1 & Card 2
-  const handleSelect = (indexToRemove: number) => {
-    setTournament((prevArray) => {
-      const itemToRemove = prevArray[indexToRemove].title;
-      const updatedArray = prevArray.filter(
-        (element) => element.title !== itemToRemove
-      );
+  // Tag to Color mapping
+  useEffect(() => {
+    const map: Record<string, string> = {};
+    let colorIndex = 0;
 
-      if (updatedArray.length === 1) {
-        setWinner(true);
-      } else {
-        const newLength = updatedArray.length;
-
-        setFirstIndex((prev) => Math.min(prev, newLength - 1));
-        setSecondIndex((prev) => Math.min(prev, newLength - 1));
-      }
-      // return updatedArray to update the state for tournament
-      return updatedArray;
+    restaurants.forEach((restaurant) => {
+      restaurant.tags.split(",").forEach((rawTag) => {
+        const tag = rawTag.trim();
+        if (!map[tag]) {
+          map[tag] = colors[colorIndex % colors.length];
+          colorIndex++;
+        }
+      });
     });
-  };
+    setTagColorMap(map);
+  }, [restaurants]);
 
   return (
     // Background
@@ -155,9 +255,12 @@ export const LastBiteStanding = () => {
           winner ? (
             // Winner
             <div className="relative flex justify-center min-h-screen items-center space-x-12">
-              <div
-                className="w-108 h-[36rem] bg-cover rounded-xl hover:bg-gray-200 active:scale-105 p-0 flex flex-col justify-end text-left"
-                style={{ backgroundImage: `url(${tournament[0].image})` }}
+              <button
+                onClick={() => handleSelect(secondIndex)}
+                className="w-108 h-[36rem] bg-cover rounded-xl hover:scale-105 active:scale-100 p-0 flex flex-col justify-end text-left"
+                style={{
+                  backgroundImage: `url(${tournament[0].image})`,
+                }}
               >
                 <div className="p-4 rounded-lg bg-black/30 backdrop-blur">
                   <h1 className="text-4xl font-bold mb-4 text-white text-shadow-lg">
@@ -166,11 +269,21 @@ export const LastBiteStanding = () => {
                   <p className="text-xl mb-2 text-white text-shadow-lg">
                     Rating: {tournament[0].rating}
                   </p>
-                  <p className="text-lg text-white text-shadow-lg">
-                    {tournament[0].tags}
-                  </p>
+                  <div className="space-x-4">
+                    {tournament[0]?.tags.split(",").map((rawTag) => {
+                      const tag = rawTag.trim();
+                      const color = tagColorMap[tag];
+                      return (
+                        <p
+                          className={`inline-block ${color} px-2 font-semibold text-white hover:scale-105 transition rounded-xl`}
+                        >
+                          {tag}
+                        </p>
+                      );
+                    })}
+                  </div>
                 </div>
-              </div>
+              </button>
             </div>
           ) : (
             <>
@@ -179,9 +292,21 @@ export const LastBiteStanding = () => {
                   <div>
                     {tournament.map((tournament, index) => (
                       <div key={index}>
-                        <h2 className="bg-white border ">{tournament.title}</h2>
+                        <h2 className="bg-white border ">
+                          {tournament.tags.split(",")}
+                        </h2>
                       </div>
                     ))}
+                    <div>
+                      {Object.entries(tagColorMap).map(([tag, color], i) => (
+                        <p
+                          key={i}
+                          className={`${color} px-2 rounded-xl inline-block`}
+                        >
+                          {tag}
+                        </p>
+                      ))}
+                    </div>
                   </div>
                   {/* TEST Card 1 */}
                   <button
@@ -194,14 +319,25 @@ export const LastBiteStanding = () => {
                     <div className="p-4 rounded-lg bg-black/30 backdrop-blur">
                       <h1 className="text-4xl font-bold mb-4 text-white text-shadow-lg">
                         {tournament[firstIndex].title}
-                        {firstIndex}
                       </h1>
                       <p className="text-xl mb-2 text-white text-shadow-lg">
                         Rating: {tournament[firstIndex].rating}
                       </p>
-                      <p className="text-lg text-white text-shadow-lg">
-                        {tournament[firstIndex].tags}
-                      </p>
+                      <div className="border space-x-4">
+                        {tournament[firstIndex]?.tags
+                          .split(",")
+                          .map((rawTag) => {
+                            const tag = rawTag.trim();
+                            const color = tagColorMap[tag];
+                            return (
+                              <p
+                                className={`inline-block ${color} px-2 rounded-xl`}
+                              >
+                                {tag}
+                              </p>
+                            );
+                          })}
+                      </div>
                     </div>
                   </button>
 
@@ -216,14 +352,25 @@ export const LastBiteStanding = () => {
                     <div className="p-4 rounded-lg bg-black/30 backdrop-blur">
                       <h1 className="text-4xl font-bold mb-4 text-white text-shadow-lg">
                         {tournament[secondIndex].title}
-                        {secondIndex}
                       </h1>
                       <p className="text-xl mb-2 text-white text-shadow-lg">
                         Rating: {tournament[secondIndex].rating}
                       </p>
-                      <p className="text-lg text-white text-shadow-lg">
-                        {tournament[secondIndex].tags}
-                      </p>
+                      <div className="border space-x-4">
+                        {tournament[secondIndex]?.tags
+                          .split(",")
+                          .map((rawTag) => {
+                            const tag = rawTag.trim();
+                            const color = tagColorMap[tag];
+                            return (
+                              <p
+                                className={`inline-block ${color} px-2 rounded-xl`}
+                              >
+                                {tag}
+                              </p>
+                            );
+                          })}
+                      </div>
                     </div>
                   </button>
                 </div>
@@ -232,7 +379,7 @@ export const LastBiteStanding = () => {
                   {/* Card 1 */}
                   <button
                     onClick={() => handleSelect(secondIndex)}
-                    className="w-108 h-[36rem] bg-cover rounded-xl hover:bg-gray-200 active:scale-105 p-0 flex flex-col justify-end text-left"
+                    className="w-108 h-[36rem] bg-cover rounded-xl hover:scale-105 active:scale-100 p-0 flex flex-col justify-end text-left"
                     style={{
                       backgroundImage: `url(${tournament[firstIndex].image})`,
                     }}
@@ -240,21 +387,32 @@ export const LastBiteStanding = () => {
                     <div className="p-4 rounded-lg bg-black/30 backdrop-blur">
                       <h1 className="text-4xl font-bold mb-4 text-white text-shadow-lg">
                         {tournament[firstIndex].title}
-                        {firstIndex}
                       </h1>
                       <p className="text-xl mb-2 text-white text-shadow-lg">
                         Rating: {tournament[firstIndex].rating}
                       </p>
-                      <p className="text-lg text-white text-shadow-lg">
-                        {tournament[firstIndex].tags}
-                      </p>
+                      <div className="space-x-4">
+                        {tournament[firstIndex]?.tags
+                          .split(",")
+                          .map((rawTag) => {
+                            const tag = rawTag.trim();
+                            const color = tagColorMap[tag];
+                            return (
+                              <p
+                                className={`inline-block ${color} px-2 font-semibold text-white hover:scale-105 transition rounded-xl`}
+                              >
+                                {tag}
+                              </p>
+                            );
+                          })}
+                      </div>
                     </div>
                   </button>
 
                   {/* Card 2 */}
                   <button
                     onClick={() => handleSelect(firstIndex)}
-                    className="w-108 h-[36rem] bg-cover bg-white rounded-xl text-center hover:bg-gray-200 active:scale-105 p-0 flex flex-col justify-end text-left"
+                    className="w-108 h-[36rem] bg-cover rounded-xl hover:scale-105 active:scale-100 p-0 flex flex-col justify-end text-left"
                     style={{
                       backgroundImage: `url(${tournament[secondIndex].image})`,
                     }}
@@ -262,14 +420,25 @@ export const LastBiteStanding = () => {
                     <div className="p-4 rounded-lg bg-black/30 backdrop-blur">
                       <h1 className="text-4xl font-bold mb-4 text-white text-shadow-lg">
                         {tournament[secondIndex].title}
-                        {secondIndex}
                       </h1>
                       <p className="text-xl mb-2 text-white text-shadow-lg">
                         Rating: {tournament[secondIndex].rating}
                       </p>
-                      <p className="text-lg text-white text-shadow-lg">
-                        {tournament[secondIndex].tags}
-                      </p>
+                      <div className="space-x-4">
+                        {tournament[secondIndex]?.tags
+                          .split(",")
+                          .map((rawTag) => {
+                            const tag = rawTag.trim();
+                            const color = tagColorMap[tag];
+                            return (
+                              <p
+                                className={`inline-block ${color} px-2 font-semibold text-white hover:scale-105 transition rounded-xl`}
+                              >
+                                {tag}
+                              </p>
+                            );
+                          })}
+                      </div>
                     </div>
                   </button>
                 </div>
